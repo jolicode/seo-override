301e93bc36a4590400ea32e19135a82469014c03
Damien ALEXANDRE <mobman02@gmail.com>
2017-04-07 13:54:09 +0200

    Add Response subscriber and override method in SeoManager

---
+++ src/Bridge/Symfony/Subscriber/SeoSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the SeoOverride project.
+ *
+ * (c) JoliCode <coucou@jolicode.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Joli\SeoOverride\Bridge\Symfony\Subscriber;
+
+use Joli\SeoOverride\SeoManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class SeoSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var SeoManager
+     */
+    private $seoManager;
+
+    public function __construct(SeoManager $seoManager)
+    {
+        $this->seoManager = $seoManager;
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if ($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) {
+            $path = $event->getRequest()->getPathInfo();
+
+            $this->seoManager->fetchSeoForPath($path);
+
+            $responseContent = $event->getResponse()->getContent();
+            $newResponseContent = $this->seoManager->overrideHtml($responseContent);
+
+            $event->getResponse()->setContent($newResponseContent);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => [
+                ['onKernelResponse', 0],
+            ],
+        ];
+    }
+}
--- src/SeoManager.php
+++ src/SeoManager.php
@@ -40,4 +40,81 @@ class SeoManager
     {
         return $this->seo;
     }
+
+    /**
+     * Update the Seo from the fetchers for a specific path.
+     *
+     * @param mixed $path
+     *
+     * @return Seo
+     */
+    public function fetchSeoForPath($path)
+    {
+        // @todo
+        return $this->seo;
+    }
+
+    /**
+     * Perform the override of HTML SEO related tags.
+     *
+     * @param $html
+     *
+     * @return bool|Seo
+     */
+    public function overrideHtml($html)
+    {
+        // @todo
+
+        $seo = $this->getSeo();
+
+        if ($seo->getMetaTitle()) {
+            $html = preg_replace(
+                '@<!--SEO_TITLE-->.+<!--/SEO_TITLE-->@im',
+                '<title>'.htmlspecialchars($seo->getMetaTitle()).'</title>',
+                $html
+            );
+        }
+
+        if ($seo->getMetaDescription()) {
+            $html = preg_replace(
+                '@<!--SEO_DESC-->.*?<!--/SEO_DESC-->@im',
+                '<meta name="description" content="'.htmlspecialchars($seo->getMetaDescription()).'">',
+                $html
+            );
+        }
+
+        if ($seo->getMetaCanonical()) {
+            $html = preg_replace(
+                '@<!--SEO_CANO-->.*?<!--/SEO_CANO-->@im',
+                '<link rel="canonical" href="'.htmlspecialchars($seo->getMetaCanonical()).'" />',
+                $html
+            );
+        }
+
+        if ($seo->getMetaRobots()) {
+            $html = preg_replace(
+                '@<!--SEO_ROBOT-->.*?<!--/SEO_ROBOT-->@im',
+                '<meta name="robots" content="'.htmlspecialchars($seo->getMetaRobots()).'">',
+                $html
+            );
+        }
+
+        if ($seo->getOgTitle()) {
+            $html = preg_replace(
+                '@<!--SEO_OGTITLE-->.*?<!--/SEO_OGTITLE-->@im',
+                '<meta property="og:title" content="'.htmlspecialchars($seo->getOgTitle()).'" />',
+                $html
+            );
+        }
+
+        if ($seo->getOgDescription()) {
+            $html = preg_replace(
+                '@<!--SEO_OGDESC-->.*?<!--/SEO_OGDESC-->@im',
+                '<meta property="og:description" content="'.htmlspecialchars($seo->getOgDescription()).'" />',
+                $html
+            );
+        }
+
+        return $html;
+    }
 }
